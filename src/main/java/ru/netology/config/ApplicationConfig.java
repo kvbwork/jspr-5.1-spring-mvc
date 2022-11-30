97cc725274341032e6b583956dcbfa48d0665a5d
kvbwork <kvbdev@yandex.ru>
2022-11-30 18:19:28 +0500

    ApplicationConfig implemented

---
+++ src/main/java/ru/netology/config/ApplicationConfig.java
@@ -0,0 +1,90 @@
+package ru.netology.config;
+
+import ru.netology.controller.PostController;
+import ru.netology.exception.NotFoundException;
+import ru.netology.repository.PostRepository;
+import ru.netology.service.PostService;
+import ru.netology.servlet.HandlerMapping;
+
+import java.util.Optional;
+
+public class ApplicationConfig {
+    private static volatile ApplicationConfig INSTANCE;
+
+    protected static final String GET = "GET";
+    protected static final String POST = "POST";
+    protected static final String DELETE = "DELETE";
+
+    private final HandlerMapping handlerMapping;
+    private final PostRepository postRepository;
+    private final PostService postService;
+    private final PostController postController;
+
+
+    private ApplicationConfig() {
+        postRepository = new PostRepository();
+        postService = new PostService(postRepository);
+        postController = new PostController(postService);
+        handlerMapping = new HandlerMapping();
+
+        configureRoutes(handlerMapping);
+    }
+
+    protected void configureRoutes(HandlerMapping handlers) {
+        handlers.addStaticPathHandler(GET, "/api/posts", (req, resp) -> {
+            postController.all(resp);
+        });
+        handlers.addPatternPathHandler(GET, "/api/posts/\\d+", (req, resp) -> {
+            postController.getById(
+                    getIdPathParam(req.getServletPath()).orElseThrow(NotFoundException::new), resp);
+        });
+        handlers.addStaticPathHandler(POST, "/api/posts", (req, resp) -> {
+            postController.save(req.getReader(), resp);
+        });
+        handlers.addPatternPathHandler(DELETE, "/api/posts/\\d+", (req, resp) -> {
+            postController.removeById(
+                    getIdPathParam(req.getServletPath()).orElseThrow(NotFoundException::new), resp);
+        });
+    }
+
+    public static ApplicationConfig getInstance() {
+        if (INSTANCE == null) {
+            synchronized (ApplicationConfig.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = new ApplicationConfig();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
+    public void dispose() {
+        INSTANCE = null;
+    }
+
+    private static Optional<Long> getIdPathParam(String path) {
+        final var lastSlashIndex = path.lastIndexOf("/");
+        if (lastSlashIndex == -1 || lastSlashIndex >= path.length()) return Optional.empty();
+        try {
+            return Optional.of(Long.parseLong(path.substring(lastSlashIndex + 1)));
+        } catch (NumberFormatException ex) {
+            return Optional.empty();
+        }
+    }
+
+    public HandlerMapping getHandlerMapping() {
+        return handlerMapping;
+    }
+
+    public PostRepository getPostRepository() {
+        return postRepository;
+    }
+
+    public PostService getPostService() {
+        return postService;
+    }
+
+    public PostController getPostController() {
+        return postController;
+    }
+}
