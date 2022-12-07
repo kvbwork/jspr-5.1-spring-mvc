f7dc8748deb023d3b5ce4170ed5df667e41ad214
kvbwork <kvbdev@yandex.ru>
2022-12-07 22:26:55 +0500

    PostDto implemented

---
+++ src/main/java/ru/netology/dto/PostDto.java
@@ -0,0 +1,31 @@
+package ru.netology.dto;
+
+public class PostDto {
+  private long id;
+  private String content;
+
+  public PostDto() {
+  }
+
+  public PostDto(long id, String content) {
+    this.id = id;
+    this.content = content;
+  }
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public String getContent() {
+    return content;
+  }
+
+  public void setContent(String content) {
+    this.content = content;
+  }
+
+}
