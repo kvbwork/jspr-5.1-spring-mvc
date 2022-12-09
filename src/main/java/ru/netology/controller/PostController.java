c0fb511007da56b1965c8df8b6c86e6256c93d48
kvbwork <kvbdev@yandex.ru>
2022-12-09 20:51:23 +0500

    remove logic from PostController

--- src/main/java/ru/netology/controller/PostController.java
+++ src/main/java/ru/netology/controller/PostController.java
@@ -2,45 +2,32 @@ package ru.netology.controller;
 
 import org.springframework.web.bind.annotation.*;
 import ru.netology.dto.PostDto;
-import ru.netology.exception.NotFoundException;
-import ru.netology.mapper.PostMapper;
 import ru.netology.service.PostService;
 
 import java.util.List;
-import java.util.stream.Collectors;
-
-import static java.util.Optional.of;
 
 @RestController
 @RequestMapping("/api/posts")
 public class PostController {
     private final PostService service;
-    private final PostMapper postMapper;
 
-    public PostController(PostService service, PostMapper postMapper) {
+    public PostController(PostService service) {
         this.service = service;
-        this.postMapper = postMapper;
     }
 
     @GetMapping
     public List<PostDto> all() {
-        return service.all().stream()
-                .map(postMapper::postToPostDto)
-                .collect(Collectors.toList());
+        return service.all();
     }
 
     @GetMapping("/{id}")
     public PostDto getById(@PathVariable long id) {
-        return postMapper.postToPostDto(service.getById(id));
+        return service.getById(id);
     }
 
     @PostMapping
-    public PostDto save(@RequestBody PostDto postDto) {
-        return of(postDto)
-                .map(postMapper::postDtoToPost)
-                .map(service::save)
-                .map(postMapper::postToPostDto)
-                .orElseThrow(NotFoundException::new);
+    public PostDto save(@RequestBody PostDto post) {
+        return service.save(post);
     }
 
     @DeleteMapping("/{id}")
